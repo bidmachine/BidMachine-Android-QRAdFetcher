bb7745616612165213cc80f499de0a7b0f1fcc55
nikita_cherep <DemonD600@mail.ru>
2020-07-10 16:12:07 +0300

    Added WebView debuggable true

---
+++ app/src/main/java/io/bidmachine/qr_ad_fetcher/App.kt
@@ -0,0 +1,13 @@
+package io.bidmachine.qr_ad_fetcher
+
+import android.app.Application
+import android.webkit.WebView
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        WebView.setWebContentsDebuggingEnabled(true)
+    }
+}
